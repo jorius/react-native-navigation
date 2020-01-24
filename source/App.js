a7e40c72a30fd127202f85e93709e69289697f71
Jorius <josedrc97@gmail.com>
2020-01-24 11:05:11 -0500

    Refactor entry point

--- source/App.js
+++ source/App.js
@@ -1,19 +1,7 @@
+// @packages
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+// @scripts
+import AppContainer from './containers/app';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => <AppContainer />;
